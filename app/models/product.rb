19691d5208c0626fd40368826789095b65ed2bec
Tam <nvtamnt99@gmail.com>
2019-12-29 11:19:12 +0700

    refactor code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  validates :product_name, length: { is: 4 }
+  validates :category_id, presence: true
+  validates :product_id, presence: true
+  validates :discontinued, presence: true
+  validates_with CategoryValidator
 end
