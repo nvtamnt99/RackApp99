e3bde7a8bb0ef5e637cbd2518a4b41ce6bf74632
Tam <nvtamnt99@gmail.com>
2020-01-05 20:15:46 +0700

    refactor code file routes

--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Frack
   class Router
     ROUTES = [
@@ -10,42 +12,37 @@ module Frack
         request_method: 'get',
         request_path: '/sign_up',
         mapping: 'UsersController#new'
-  },
+      },
       {
         request_method: 'post',
         request_path: '/sign_up',
         mapping: 'UsersController#create'
-      },
-      {
-        '/' => 'WelcomesController#index',
-        '/users' => 'UsersController#index',
-        '/sign_up' => 'UsersController#new'
       }
-  ]
+    ].freeze
 
     def initialize(app)
       @app = app
     end
 
     def call(env)
-      request_method = env['REQUEST_METHOD'].downcase
+      request_method = env['REQUEST_METHOD']
       request_path = env['PATH_INFO']
 
-      ROUTES = ROUTES.find do |i|
+      route = ROUTES.find do |i|
         i[:request_method] == request_method &&
-        i[:request_path] == request_path
+          i[:request_path] == request_path
       end
 
       if (mapping = ROUTES[env['PATH_INFO']])
         env.merge!(controller_action(mapping))
         @app.call(env)
       else
-        Rack::Response.new('Not found',404)
+        Rack::Response.new('Not found', 404)
       end
     end
 
     def controller_action(mapping)
-      Hash[ %w(controller action).zip mapping.split('#')]
+      Hash[%w[controller action].zip mapping.split('#')]
     end
   end
 end
