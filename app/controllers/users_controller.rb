2a2cb16afb9f143333c6193a06ff76d79b925dcc
Tam <nvtamnt99@gmail.com>
2020-01-05 20:07:29 +0700

    refactor code routes and method create in UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < Frack::BaseController
     @user = User.new
     render("users/new")
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect('/')
+    else
+      render('users/new')
+    end
+  end
+
+  private
+
+  def user_params
+    request.params['user']
+  end
 end
--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -1,16 +1,41 @@
 module Frack
   class Router
-    ROUTES = {
-      '/' => 'WelcomesController#index',
-      '/users' => 'UsersController#index',
-      '/sign_up' => 'UsersController#new'
-    }
+    ROUTES = [
+      {
+        request_method: 'get',
+        request_path: '/',
+        mapping: 'UsersController#new'
+      },
+      {
+        request_method: 'get',
+        request_path: '/sign_up',
+        mapping: 'UsersController#new'
+  },
+      {
+        request_method: 'post',
+        request_path: '/sign_up',
+        mapping: 'UsersController#create'
+      },
+      {
+        '/' => 'WelcomesController#index',
+        '/users' => 'UsersController#index',
+        '/sign_up' => 'UsersController#new'
+      }
+  ]
 
     def initialize(app)
       @app = app
     end
 
     def call(env)
+      request_method = env['REQUEST_METHOD'].downcase
+      request_path = env['PATH_INFO']
+
+      ROUTES = ROUTES.find do |i|
+        i[:request_method] == request_method &&
+        i[:request_path] == request_path
+      end
+
       if (mapping = ROUTES[env['PATH_INFO']])
         env.merge!(controller_action(mapping))
         @app.call(env)
